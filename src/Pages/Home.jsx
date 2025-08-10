b764998537bd7103622c527c7ce1ef656f775d36
imanolperez9 <trabajoperez2006@gmail.com>
2025-08-10 17:22:10 -0300

    add:products de la fakestore api en el home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,23 @@
+import  { useEffect, useState } from "react"
+
 import  { Layout } from "../Components/Layout"
 
 const Home = () =>  {
+  const [products , setProducts] = useState([])
+
+const fetchingProducts = async () =>  {
+  const response = await  fetch("https://fakestoreapi.com/products" ,  {method : "GET"})
+const data = await response.json()
+
+setProducts(data)
+}
+
+useEffect(() => {
+  fetchingProducts()
+},[])
+
+
+
     return(
         <Layout >
              <div>
@@ -13,7 +30,16 @@ const Home = () =>  {
         <h1>Joyas Exclusivas</h1>
         <p>Descubrí nuestras joyas seleccionadas para realzar tu estilo. Piezas únicas, pensadas para cada ocasión: desde lo cotidiano hasta lo más sofisticado.</p>
       </section>
-
+<section>
+        {
+          products.map((product) =>  <div>
+            <h2 key={product.id}>{product.title }</h2>
+            <img src= {product.image} alt={`Imagen ${product.title}` }/>
+            <h3><strong>{product.description }</strong></h3>
+            <p>${product.price }</p>
+          </div>   ) 
+        }
+      </section>
       <section>
         <h2>ELEGINOS..</h2>
        
@@ -22,18 +48,20 @@ const Home = () =>  {
                 <h2>Promociones y Beneficios</h2>
                  <p>Aprovechá nuestras promociones especiales, descuentos por temporada y beneficios exclusivos para clientes frecuentes.</p>
             </li>
-            <h2>Envíos a todo el país y opciones de pago flexibles.</h2>
-            <li>
-                
-            </li>
+
+           <li>
+             <h2>Envíos a todo el país y opciones de pago flexibles.</h2>
+           </li>
+             
         </ul>
       </section>
 
       <section>
         <h2>Comprá con Confianza</h2>
         <p>En tienda Lincoln nos enfocamos en brindarte una experiencia de compra segura, cómoda y confiable.</p>
-        <p>Te acompañamos en cada paso de tu compra.</p>
+       
       </section>
+      
     </div>
 
         </Layout>
